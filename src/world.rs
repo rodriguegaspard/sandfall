24b4a0e1af4bd098496145a9f62bdea2be997887
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2024-03-24 22:15:50 +0800

    Added a is_leaf method

--- src/world.rs
+++ src/world.rs
@@ -41,6 +41,10 @@ impl ParticleWorld {
             self._quadrants[3] = Some(Box::new(ParticleWorld::new(None, (self._boundaries.0+self._boundaries.2/2, self._boundaries.1+self._boundaries.3/2, self._boundaries.2, self._boundaries.3))));
     }
 
+    pub fn is_leaf(&self) -> bool {
+        self._quadrants[0].is_none()
+    }
+
     pub fn contains_coords(&self, x: u32, y: u32) -> bool {
         (self._boundaries.0 <= x) && (self._boundaries.1 <= y) && (self._boundaries.2 >= x) && (self._boundaries.3 >= y)
     } 
