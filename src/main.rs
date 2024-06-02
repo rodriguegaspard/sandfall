49d4efe78eed3fbb5cb77b6d0e2d62087db73d61
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2024-06-02 20:00:53 +0800

    Modified test

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ use crate::particle::Particle;
 fn main() {
     let mut w = ParticleWorld::new(None, (0, 0, 800, 800));
     let p = Particle::new("water".to_string());
-    w.insert(p, 100, 100);
+    w.insert(p, 51, 50);
 }
