f68417d7a3f7d1e993bbdf092cb26f7248d329e9
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2024-06-20 01:52:58 +0800

    Changed example and experimented with different world sizes

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use crate::world::ParticleWorld;
 use crate::particle::Particle;
 
 fn main() {
-    let mut w = ParticleWorld::new(None, (0, 0, 800, 800));
+    let mut w = ParticleWorld::new(None, (0, 0, 1200, 800));
     let p1 = Particle::new("water".to_string());
     let p2 = Particle::new("oil".to_string());
     let p3 = Particle::new("lava".to_string());
     let p4 = Particle::new("sand".to_string());
-    w.insert(p1, 0, 0);
-    w.insert(p2, 0, 799);
-    w.insert(p3, 799, 0);
-    w.insert(p4, 799, 799);
+    w.insert(p1, 1, 1);
+    w.insert(p2, 0, 0);
+    w.insert(p3, 0, 1);
+    w.insert(p4, 1, 0);
 }
