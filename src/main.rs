1de8eca8934a68d34fb6a73d5cdb8d086cdbc507
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2024-06-19 22:17:07 +0800

    Added some examples for insert()

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ use crate::particle::Particle;
 
 fn main() {
     let mut w = ParticleWorld::new(None, (0, 0, 800, 800));
-    let p = Particle::new("water".to_string());
-    w.insert(p, 51, 50);
+    let p1 = Particle::new("water".to_string());
+    let p2 = Particle::new("oil".to_string());
+    let p3 = Particle::new("lava".to_string());
+    let p4 = Particle::new("sand".to_string());
+    w.insert(p1, 0, 0);
+    w.insert(p2, 0, 799);
+    w.insert(p3, 799, 0);
+    w.insert(p4, 799, 799);
 }
