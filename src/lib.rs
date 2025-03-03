9f9072abf1c854c796b4249b22217896a265ff47
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-03-03 14:53:58 +0800

    Small example in lib.rs that draw quadrants to a <canvas>

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,40 @@
+pub mod world;
+pub mod particle;
+pub mod element;
+pub mod render;
+
+use web_sys::HtmlCanvasElement;
+use web_sys::CanvasRenderingContext2d;
+use crate::world::{ParticleWorld, Square};
+use crate::particle::Particle;
+use crate::render::Renderer;
+use render::QuadrantRenderer;
+
 use wasm_bindgen::prelude::*;
 
-// Called when the Wasm module is instantiated
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+    // HTML <canvas> initialization
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<HtmlCanvasElement>()
+        .unwrap();
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()
+        .unwrap();
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
-
+    // World initialization
+    let mut w = ParticleWorld::new(Square::new(0.0, 0.0, 800.0));
+    let p = Particle::new(1);
+    w.insert(p, 250.0, 350.0);
+    let mut quadrants = Vec::new(); 
+    w.get_quadrants(&mut quadrants);
+    let qr = QuadrantRenderer;
+    qr.draw(&quadrants, &context);
     Ok(())
-}
-
-#[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
 }
