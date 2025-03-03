cd741be0128e257fb2f89f52ef6c786fe5520494
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-03-03 14:53:18 +0800

    Modified main.rs to reflect the changes made to the coord system of ParticleWorld

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,19 @@ pub mod world;
 pub mod particle;
 pub mod element;
 
-use crate::element::ElementTable;
-use crate::world::ParticleWorld;
+use crate::world::*;
 use crate::particle::Particle;
+use crate::element::ElementTable;
 
 fn main() {
-    let mut w = ParticleWorld::new(None, (0, 0, 1200, 800));
-    let elements = ElementTable::default();
+    let mut w = ParticleWorld::new(Square::new(0.0, 0.0, 800.0));
+    let e = ElementTable::default(); 
     let p1 = Particle::new(4);
     let p2 = Particle::new(1);
     let p3 = Particle::new(2);
     let p4 = Particle::new(3);
-    w.insert(p1, 1, 1);
-    w.insert(p2, 0, 0);
-    w.insert(p3, 0, 1);
-    w.insert(p4, 1, 0);
-    println!("{}", w.search(1,1).expect("Whoopsie").element(&elements));
-    w.search(0,1).expect("Whoopsie");
-    w.search(1,0).expect("Whoopsie");
-    w.search(1,1).expect("Whoopsie");
+    w.insert(p1, 0.0, 0.0);
+    w.insert(p2, 0.0, 0.0);
+    w.insert(p3, 1.0, 0.0);
+    w.insert(p4, 0.0, 1.0);
 }
