d30bec573e4e5953f486f85ef3955ad4faf09633
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-03-04 15:26:21 +0800

    Cleaned main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,7 @@ pub mod element;
 
 use crate::world::*;
 use crate::particle::Particle;
-use crate::element::ElementTable;
 
 fn main() {
-    let mut w = ParticleWorld::new(Square::new(0.0, 0.0, 800.0));
-    let e = ElementTable::default(); 
-    let p1 = Particle::new(4);
-    let p2 = Particle::new(1);
-    let p3 = Particle::new(2);
-    let p4 = Particle::new(3);
-    w.insert(p1, 0.0, 0.0);
-    w.insert(p2, 0.0, 0.0);
-    w.insert(p3, 1.0, 0.0);
-    w.insert(p4, 0.0, 1.0);
+    //let mut w = ParticleWorld::new(Square::new(0.0, 0.0, 800.0));
 }
