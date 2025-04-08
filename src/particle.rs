c464f1f1e17ba50100b5c822e1aa057ae042a77a
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-04-08 17:23:56 +0800

    Added two new booleans to Particle, _updated and _deleted, with associated functions

--- src/particle.rs
+++ src/particle.rs
@@ -1,19 +1,47 @@
 use wasm_bindgen::prelude::*;
-use crate::element::ElementTable;
-
-
 
 #[wasm_bindgen]
+#[derive(Copy, Clone)]
 pub struct Particle{
     _id: usize,
+    _updated: bool,
+    _deleted: bool
 }
 
 impl Particle{
     pub fn new(_id: usize) -> Particle {
-        Particle { _id }
+        Particle { 
+            _id, 
+            _updated: false, 
+            _deleted: false
+        }
     }
 
-    pub fn element(&self, elements: &ElementTable) -> String {
-        elements.name(self._id).to_string()
+    pub fn empty() -> Particle {
+        Particle {
+        _id: usize::MAX,
+        _updated: false,
+        _deleted: false
+        }
+    }
+
+    pub fn id(&self) -> usize {
+        self._id
+    }
+
+    pub fn is_updated(&self) -> bool {
+        self._updated
+    }
+
+    pub fn is_deleted(&self) -> bool {
+        self._deleted
+    }
+
+    pub fn update(&mut self){
+        self._updated = !self._updated;
+    }
+
+    pub fn delete(&mut self){
+        self._deleted = true
     }
 }
